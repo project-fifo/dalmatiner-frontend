ccc13caf785d6891d81a8af836f3de83ea05dc76
Tomasz Szarstuk <szarsti@gmail.com>
2016-06-17 19:27:37 +0100

    Add query authorization

--- apps/dalmatiner_frontend/src/dalmatiner_dl_authz_m.erl
+++ apps/dalmatiner_frontend/src/dalmatiner_dl_authz_m.erl
@@ -34,6 +34,9 @@ authorize(Req, Env) ->
             {ok, Req1};
         {deny, Req1} ->
             {ok, Req2} = cowboy_req:reply(403, Req1),
+            {halt, Req2};
+        {error, Status, Error, Req1} ->
+            {ok, Req2} = cowboy_req:reply(Status, [], Error, Req1),
             {halt, Req2}
     end.
 
@@ -54,6 +57,13 @@ match({path, Pattern}, Req) ->
             {true, Req1};
         _ ->
             {false, Req1}
+    end;
+match({param, Name}, Req) ->
+    case cowboy_req:qs_val(Name, Req) of
+        {undefined, Req1} ->
+            {false, Req1};
+        {_, Req1} ->
+            {true, Req1}
     end.
 
 match_binary(Subject, Pattern) when size(Pattern) =< size(Subject) ->
@@ -81,4 +91,49 @@ assert(require_collection_access, Req) ->
             {Access, Req2};
         {_, Req1} ->
             {deny, Req2}
+    end;
+assert(require_query_collection_access, Req) ->
+    {Q, Req1} =  cowboy_req:qs_val(<<"q">>, Req),
+    case dql:prepare(Q) of
+        {ok, {Parts, _Start, _Count, _Res, Aliases, _Buckets}} ->
+            {UserId, Req2} = cowboy_req:meta(dl_auth_user, Req1),
+            {ok, Orgs} = dalmatiner_dl_data:user_orgs(UserId),
+            OrgOidMap = lists:foldl(fun (O, Acc) ->
+                                            Acc#{maps:get(<<"_id">>, O) => allow}
+                                   end, #{}, Orgs),
+            AList = gb_trees:values(Aliases),
+            Access = check_query_access(Parts ++ AList, OrgOidMap),
+            {Access, Req2};
+        {error, E} ->
+            Error = list_to_binary(dqe:error_string({error, E})),
+            lager:warning("Error in query validation [~s]: ~p", [Q, E]),
+            {error, 400, Error, Req1}
     end.
+
+
+check_query_access([], _) ->
+    allow;
+check_query_access([Part | Rest], OrgOidMap) ->
+    case check_query_part_access(Part, OrgOidMap) of
+        allow -> check_query_access(Rest, OrgOidMap);
+        Access -> Access
+    end.
+
+check_query_part_access({named, _N, Nested}, OrgOidMap) ->
+    check_query_part_access(Nested, OrgOidMap);
+check_query_part_access({calc, _Chain, Selector}, OrgOidMap) ->
+    check_query_part_access(Selector, OrgOidMap);
+% Always allow access to variables, because they will be checked in aliases section
+check_query_part_access({var, _Name}, _OrgOidMap) ->
+    % TODO: Need to test access rights with aliases
+    allow;
+% Right now access by bucket is groupped and finger is first segment of metric
+check_query_part_access({get, {_Bucket, Metric}}, OrgOidMap) ->
+    Finger = hd(Metric),
+    OrgOids = maps:keys(OrgOidMap),
+    {ok, Access} = dalmatiner_dl_data:agent_access(Finger, OrgOids),
+    Access;
+% Mapping by colleciton already uses collection as org id
+check_query_part_access({lookup, {in, Collection, _Metric}}, OrgOidMap) ->
+    Oid = {base16:decode(Collection)},
+    maps:get(Oid, OrgOidMap, deny).
--- apps/dalmatiner_frontend/src/dalmatiner_dl_data.erl
+++ apps/dalmatiner_frontend/src/dalmatiner_dl_data.erl
@@ -5,7 +5,7 @@
 
 %% API
 -export([start_link/1]).
--export([user_orgs/1, user_org_access/2]).
+-export([user_orgs/1, user_org_access/2, agent_access/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -15,6 +15,8 @@
 -define(TIMEOUT, 30000).
 -record(state, {connection}).
 
+%% TODO: Add debug timing arround DB queries
+
 %% ===================================================================
 %% API functions
 %% ===================================================================
@@ -22,14 +24,17 @@
 start_link(Args) ->
     gen_server:start_link(?MODULE, Args, []).
 
--spec user_orgs(<<_:96>>) -> {ok, [#{}]}.
+-spec user_orgs(binary()) -> {ok, [#{}]}.
 user_orgs(UserId) ->
     call({user_orgs, UserId}).
 
--spec user_org_access(<<_:96>>, <<_:96>>) -> {ok, allow | deny}.
+-spec user_org_access(binary(), binary()) -> {ok, allow | deny}.
 user_org_access(UserId, OrgId) ->
     call({user_org_access, UserId, OrgId}).
-    
+
+-spec agent_access(binary(), [{<<_:96>>}]) -> {ok, allow | deny}.
+agent_access(Finger, OrgOids) ->
+    call({agent_access, Finger, OrgOids}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -50,6 +55,9 @@ handle_call({user_orgs, UserId}, _From, #state{connection = C} = State) ->
     {reply, {ok, Orgs}, State};
 handle_call({user_org_access, UserId, OrgId}, _From, #state{connection = C} = State) ->
     Access = check_user_org_access(C, UserId, OrgId),
+    {reply, {ok, Access}, State};
+handle_call({agent_access, Finger, OrgOids}, _From, #state{connection = C} = State) ->
+    Access = check_agent_access(C, Finger, OrgOids),
     {reply, {ok, Access}, State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -118,4 +126,14 @@ check_user_org_access(C, UserId, OrgId) ->
             deny    
     end.
 
-    
+check_agent_access(C, Finger, OrgOids) ->
+    Agent = mc_worker_api:find_one(C, <<"agents">>,
+                                   {<<"_id">>, Finger,
+                                    <<"org">>, {<<"$in">>, OrgOids}},
+                                   #{projector => {<<"_id">>, true}}),
+    case Agent of
+        #{<<"_id">> := _} ->
+            allow;
+        _ ->
+            deny
+    end.
